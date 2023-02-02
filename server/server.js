30b6aff3906e9bf9dad6b4275bcf91491988cc60
Ferrois <ferrois2005@gmail.com>
2023-02-03 01:20:43 +0800

    -Modify- Cors Policy Leniency ++

--- server/server.js
+++ server/server.js
@@ -2,11 +2,23 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dbURI = process.env.dbURI;
 
 //Connect to MongoDB
-mongoose.connect(`${dbURI}`, () => console.log("Connected to MongoDB. Link: " + dbURI));
+mongoose.connect(`${dbURI}`, () =>
+  console.log("Connected to MongoDB. Link: " + dbURI)
+);
+
+//Cors Middleware
+app.use(
+  cors({
+    origin: "*",
+    methods: ["PUT", "GET", "POST", "DELETE"],
+    credentials: true,
+  })
+);
 
 //Serve static files from the current directory & Middleware
 app.use(express.static(__dirname));
@@ -15,30 +27,18 @@ app.use(express.json());
 //Paths Import
 const authRoute = require("./Routes/Auth");
 const testRoute = require("./Routes/Testing");
-const apiRoute = require("./Routes/Api")
+const apiRoute = require("./Routes/Api");
 
 //Path initialization
 app.use("/auth", authRoute);
 app.use("/testing", testRoute);
 app.use("/api", apiRoute);
 
-
 //Receive post request on the /api route
 app.get("/ping", function (req, res) {
   console.log(req.body);
   res.send("pong");
 });
-
-app.post("/batt", async (req, res) => {
-  if (!req) return res.send("No request found");
-  const { used, id } = req.body;
-  console.log(used, id);
-  res.send("Received");
-});
-
-
-
-
 
 //Listen on the port
 app.listen(port, function () {
